cf8ee4cb24f4924ec7e4048ba785a8b812976499
徐志毅 <xuzhiyi@qimao.com>
2021-02-07 17:52:46 +0800

    helloworld

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcom to Flutter'),
+        ),
+        body: new Center(child: new Text('Hello World')),
+      ),
+    );
+  }
+}
